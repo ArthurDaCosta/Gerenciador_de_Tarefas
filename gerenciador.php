d3478b51f710a9b2715fb4a3236ec23b0bc00cb1
Arthur Brixius da Costa <156153011+ArthurDaCosta@users.noreply.github.com>
2024-01-18 08:13:05 -0300

    Add files via upload

---
+++ gerenciador.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <META NAME="viewport" content="width=device-width, intial-scale=10">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+    <title>Gerenciador de Tarefas</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="header">
+        <h1>Gerenciador de Tarefas</h1>
+    </div>
+    <div class="form">
+        <form action="" method="get">
+            <label for="task_name">Tareefa:</label>
+            <input type="text" name="task_name" placeholder="Nome da Tarefa">
+            <button type="submit">Cadastrar</button>
+        </form>
+    </div>
+    <div class="separator">
+    </div>
+    <div class="list-tasks">
+        <ul>
+            <li>Tarefa 1</li>
+            <li>Tarefa 2</li>
+            <li>Tarefa 3</li>
+        </ul>
+    </div>
+    <div class="footer">
+        <p>Desenvolvido por Arthur Brixius da Costa</p>
+    </div>
+</div>
