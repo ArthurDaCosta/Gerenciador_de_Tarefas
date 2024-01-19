54db388bd62fcaa0f6bb0a25fb4b82d6bdf2d933
Arthur Brixius da Costa <156153011+ArthurDaCosta@users.noreply.github.com>
2024-01-19 07:12:14 -0300

    Add files via upload

--- task.php
+++ task.php
@@ -12,7 +12,7 @@ if ( isset($_POST['task_name'])) {
         if ( isset($_FILES['task_image'])) {
             $ext =strtolower( substr( $_FILES['task_image']['name'], -4));
             $file_name = md5( date('Y.m.d.H.i.s')) . $ext;
-            $dir = '~/Downloads/Testes/Gerenciador_de_Tarefas/uploads/';
+            $dir = 'Programas/Gerenciador/Gerenciador_de_Tarefas-2/uploads/';
 
             move_uploaded_file( $_FILES['task_images']['tmp_name'], $dir.$file_name );
         }
@@ -29,22 +29,22 @@ if ( isset($_POST['task_name'])) {
         unset($_POST['task_description']);
         unset($_POST['task_date']);
 
-        header('Location:gerenciador.php');
+        header('Location:index.php');
     } else{
         $_SESSION['message'] = "O campo não pode ser vazio.";
-        header('Location:gerenciador.php');
+        header('Location:index.php');
     } 
 }
 
 if ( isset($_GET['key'])) {
     array_splice($_SESSION['tasks'], $_GET['key'], 1);
     unset($_GET['key']);
-    header('Location:gerenciador.php');
+    header('Location:index.php');
 }
 
 if ( isset($_GET['clear'])) {
     unset($_SESSION['tasks']);
     unset($_GET['clear']);
-    header('Location:gerenciador.php');
+    header('Location:index.php');
 }
 
